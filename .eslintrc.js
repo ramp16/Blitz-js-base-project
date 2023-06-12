f96ab1d3064c3501ba62b4a60fda6134a8acba7d
Raul Martinez <ra916mp@gmail.com>
2023-06-12 11:10:56 -0500

    levantamiento base para crear un projecto en blitz.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,9 @@
-module.exports = require("@blitzjs/next/eslint")
+;(module.exports = require("@blitzjs/next/eslint")),
+  {
+    parserOptions: {
+      project: "tsconfig.json",
+      tsconfigRootDir: __dirname,
+      sourceType: "module",
+    },
+    // ... rest
+  }
